a2ac6faf6f6f1b5324c302dfd4a83c1d955bb42e
Matusal3m <matusalemdedeus@gmail.com>
2025-02-26 22:36:33 -0300

    add app on dist

--- src/classes/Server.ts
+++ src/classes/Server.ts
@@ -1,4 +1,4 @@
-import { ServerConfigsParams } from '../types/enums/ServerConfigsParams';
+import { ServerConfigsParams } from '../types/enums';
 import express, { type Application, type RequestHandler } from 'express';
 
 export class Server {
